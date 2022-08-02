8de7ae629a9bd390f93388602472c50ae85234bb
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2022-08-02 17:34:01 +0530

    8/2/22

--- elaris/settings.py
+++ elaris/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w*j8k*s3d)-mpr(&8gt%0_bp)tri2j$!c4ic50=-*b17(!9n$&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'elaris.onrender.com']
 
 
 # Application definition
