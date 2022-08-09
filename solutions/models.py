fd930148fbc8f91e7b9a319e97bdeef2d2f60e99
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2022-08-09 15:13:38 +0530

    8/9/22

--- solutions/models.py
+++ solutions/models.py
@@ -7,11 +7,19 @@ solution_type = (
 		('Normal', 'Normal'),
 	)
 
+solution_category = (
+		('Network', 'Network'),
+		('Digital Marketing ', 'Digital Marketing '),
+		('Data Science & Analytics', 'Data Science & Analytics')
+	)
+
+
 class Solution(models.Model):
 	name = models.CharField(max_length=9999)
 	desc = models.TextField()
 	img = models.ImageField(upload_to='solutions/img/')
 	solution_type = models.CharField(max_length=9999, choices=solution_type, default='Normal')	
+	solution_category = models.CharField(max_length=9999, choices=solution_category, default='Network')
 
 	def __str__(self):
-		return self.name + ' ' + self.solution_type
+		return self.name + ' ' + self.solution_category + ' ' + self.solution_type
